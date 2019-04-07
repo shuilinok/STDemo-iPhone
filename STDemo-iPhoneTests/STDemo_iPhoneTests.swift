68338d475c276e9c6830659e3d8086624c5c6906
lin.shui <lin.shui@yinyuetai.com>
2019-04-07 22:12:36 +0800

    Swift Demo

---
+++ STDemo-iPhoneTests/STDemo_iPhoneTests.swift
@@ -0,0 +1,34 @@
+//
+//  STDemo_iPhoneTests.swift
+//  STDemo-iPhoneTests
+//
+//  Created by shuilin on 4/7/19.
+//  Copyright © 2019 shuilin. All rights reserved.
+//
+
+import XCTest
+@testable import STDemo_iPhone
+
+class STDemo_iPhoneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
